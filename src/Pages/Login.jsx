30e566c840f1f6e7d0d0f518a9fe6aaf0529fbad
ShakaPursuit <shakalivngstone@pursuit.org>
2024-03-01 10:45:01 -0500

    loginComp and singupComp skelton complete and css

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React from "react";
+import toggle from "../assets/toggle.png"
+import logo from "../assets/GH.png"
+import quote from "../assets/quote.png"
 
-const Login = () => {
-    return (
-        <div>
-            
+import "../App.css";
+
+ const Login = () => {
+  return (
+    <div className="frame">
+      <div className="div">
+        <img className="updated-logo"src={logo}/>
+        <input className="text-wrapper-2" type="text" placeholder="UserName"/>
+      
+        <img  className="toggle-on-instance" src={toggle}/>
+        
+        <input className="text-wrapper-3" type="text" placeholder="Password"/>
+        <div className="overlap">
+
+         <button className="login">LOGIN</button>
         </div>
-    );
+     <div classname='center'>
+         
+       
+        <button className="sign-up">Sign-Up</button>
+
+     </div>
+      <img className="quote" src={quote}/>
+      </div>
+    </div>
+  );
 };
 
-export default Login;
+export default Login
