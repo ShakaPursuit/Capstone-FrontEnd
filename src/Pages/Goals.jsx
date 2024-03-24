3b5c48551c8b4efc6dcc4e26fa1fe3d2830aa77c
Tyrell <tyrellkamara-oliver@pursuit.org>
2024-03-24 16:49:05 -0400

    created a GoalCard component, added edit and delete functionality

--- src/Pages/Goals.jsx
+++ src/Pages/Goals.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import EditGoal from "../Components/EditGoal";
+import GoalCard from "../Components/GoalCard";
+import "./Goals.css";
 
 const Goals = ({ user, token }) => {
   const API = import.meta.env.VITE_BASE_URL;
   const [goals, setGoals] = useState([]);
   const [err, setError] = useState("");
 
-  const [editGoal, setEditGoal] = useState(false);
+  
 
   // console.log("The user obj", user);
 
@@ -35,18 +36,11 @@ const Goals = ({ user, token }) => {
 
   return (
     <div className="goals-container">
-      <h1>The Goals Page</h1>
+      {/* <h1>The Goals Page</h1> */}
 
       {goals.map((goal) => {
         return (
-          <div key={goal.goal_id} className="goalcard">
-            <h2>{goal.name}</h2>
-            <p>{goal.description}</p>
-            <p>Target Date: {goal.target_date.slice(0, 10)}</p>
-            <button onClick={() => setEditGoal(!editGoal)}>edit</button>
-            <button>Completed</button>
-            <EditGoal user={user} token={token} goalID={goal.goal_id} />
-          </div>
+          <GoalCard key={goal.goal_id} user={user} token={token} goal={goal} />
         );
       })}
     </div>
